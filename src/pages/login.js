eb6a1a0f7d0b5288fed01a44ea62480fbddd9c9c
Carvalho <carvalho.pedro.cardoso@gmail.com>
2019-10-17 00:06:25 -0300

    finish basic front

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, } from "react";
 import "./login.css";
 import api from "../services/api";
 import logo from "../assets/logo.svg";
@@ -7,13 +7,14 @@ export default function login({ history }) {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [username, setUsername] = useState("");
 
-  async function handleSubmit(e) {
+  async function handleSubmit(e) { 
     e.preventDefault();
     // console.log(username);
     const response = await api.post("/devs", { username });
     console.log(response);
     const { _id } = response.data;
     history.push(`/dev/${_id}`);
+  
   }
   return (
     <div className="login-container">
